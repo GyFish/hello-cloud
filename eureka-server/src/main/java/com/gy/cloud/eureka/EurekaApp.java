5a1d74193b42d7cc3ec34ba4fbdab003207ec452
GyFish <geyu2227@163.com>
2018-05-30 00:00:33 +0800

    init

---
+++ eureka-server/src/main/java/com/gy/cloud/eureka/EurekaApp.java
@@ -0,0 +1,18 @@
+package com.gy.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by gy on 2018/5/29.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApp.class, args);
+    }
+
+}
