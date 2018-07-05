312ff9d1205d9adaede6a101ce3333823d9d3eee
Geeyu <geyu211@qq.com>
2018-07-05 15:58:55 +0800

    // eureka

--- service-hello/src/main/java/com/gy/cloud/eureka/client/ServiceHello.java
+++ service-hello/src/main/java/com/gy/cloud/eureka/client/ServiceHello.java
@@ -3,14 +3,14 @@ package com.gy.cloud.eureka.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 声明这是一个Eureka Client
  */
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class ServiceHello {
@@ -22,10 +22,10 @@ public class ServiceHello {
   @Value("${server.port}")
   private int serverPort = 0;
 
-  @GetMapping("/api/hello")
+  @GetMapping("/greet")
   public String hello() {
     System.out.println("/hello");
-    return "Hello, Spring Cloud! My port is " + String.valueOf(serverPort);
+    return "Hello, My port is " + String.valueOf(serverPort);
   }
 
 }
--- service-hi/src/main/java/com/gy/cloud/hi/ServiceHi.java
+++ service-hi/src/main/java/com/gy/cloud/hi/ServiceHi.java
@@ -3,14 +3,14 @@ package com.gy.cloud.hi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by gy on 2018/6/3.
  */
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 @RestController
 public class ServiceHi {
