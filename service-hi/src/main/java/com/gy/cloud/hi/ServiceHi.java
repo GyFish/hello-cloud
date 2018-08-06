7eb2a1326285429cb27dc330e8307566dc8fbea3
GyFish <geyu2227@163.com>
2018-08-06 23:13:09 +0800

    consul consumer

--- service-hi/src/main/java/com/gy/cloud/hi/ServiceHi.java
+++ service-hi/src/main/java/com/gy/cloud/hi/ServiceHi.java
@@ -1,11 +1,15 @@
 package com.gy.cloud.hi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by gy on 2018/6/3.
@@ -35,4 +39,24 @@ public class ServiceHi {
         return "home...";
     }
 
+    @Autowired
+    private LoadBalancerClient loadBalancer;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 获取所有服务
+     */
+    @GetMapping("/services")
+    public Object services() {
+        return discoveryClient.getInstances("service-hello");
+    }
+
+    /**
+     * 从所有服务中选择一个服务（轮询）
+     */
+    @GetMapping("/discover")
+    public Object discover() {
+        return loadBalancer.choose("service-hello").getUri().toString();
+    }
 }
